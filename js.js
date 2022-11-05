af7c6c1e2bcbb210651d6e13e707cb4f2fb369ca
Madan Mohan <heyitsmadan@gmail.com>
2022-11-05 12:21:16 +0530

    Add code to mark a square with color

--- js.js
+++ js.js
@@ -1,9 +1,19 @@
 n=16;
 squareWidth = 640/n;
 const container = document.querySelector(".container");
+
 for(i=1;i<=n*n;i++){
     let div = document.createElement("div");
+    div.classList.add("square");
     div.style.width=`${squareWidth}px`;
     div.style.height=`${squareWidth}px`;
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll(".square")
+squares.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("black");
+    });
+})
+
