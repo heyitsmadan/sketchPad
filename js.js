4462bd750fee23547f66c2bcd50cf6f92e0c7fc4
Madan Mohan <heyitsmadan@gmail.com>
2022-11-05 12:47:39 +0530

    Add a function for a new grid request

--- js.js
+++ js.js
@@ -1,19 +1,37 @@
-n=16;
-squareWidth = 640/n;
 const container = document.querySelector(".container");
+drawGrid(16);
+paintGrid();
 
-for(i=1;i<=n*n;i++){
-    let div = document.createElement("div");
-    div.classList.add("square");
-    div.style.width=`${squareWidth}px`;
-    div.style.height=`${squareWidth}px`;
-    container.appendChild(div);
+function drawGrid(n){
+    while(container.hasChildNodes()){
+        let square = document.querySelector(".square");
+        container.removeChild(square);
+    }
+    squareWidth = 640/n;
+    for(i=1;i<=n*n;i++){
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width=`${squareWidth}px`;
+    square.style.height=`${squareWidth}px`;
+    container.appendChild(square);
+    }
 }
 
-const squares = document.querySelectorAll(".square")
-squares.forEach(square => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    });
+function paintGrid(){
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        });
+    })
+}
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    do{
+    n = prompt("What should the grid size be? (Enter a number less than 100 to not crash the browser)");
+    }while(n>100);
+    drawGrid(n);
+    paintGrid();    
 })
 
